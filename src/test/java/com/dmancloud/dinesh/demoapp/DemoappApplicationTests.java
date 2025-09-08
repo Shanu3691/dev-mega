1c12b05195e787beb80a50fefb940a7968efa7cc
eric <eric@gmail.com>
2025-09-09 01:02:04 +0530

    i have change again old code

--- src/test/java/com/dmancloud/dinesh/demoapp/DemoappApplicationTests.java
+++ src/test/java/com/dmancloud/dinesh/demoapp/DemoappApplicationTests.java
@@ -1,14 +1,13 @@
+package com.dmancloud.dinesh.demoapp;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class DemoappApplicationTests {
 
-    @Test
-    void contextLoads() {
-        // Simple assertion to ensure the test actually verifies something
-        assertTrue(true, "The application context should load without issues");
-    }
+	@Test
+	void contextLoads() {
+	}
+
 }
